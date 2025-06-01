e8760ca41fdfdcd5fca948348804c3aef19002c8
beunyeong <beunyeong.b@gmail.com>
2025-06-02 03:46:40 +0900

    Test: contextLoads 테스트 실패 해결을 위한 외부 빈(MockBean) 등록

--- src/test/java/com/example/tastefulai/TastefulAiApplicationTests.java
+++ src/test/java/com/example/tastefulai/TastefulAiApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.tastefulai;
 
+import com.example.tastefulai.domain.member.auth.CustomOAuth2UserService;
+import com.example.tastefulai.domain.member.auth.OAuth2SuccessHandler;
+import com.example.tastefulai.global.config.filter.JwtAuthFilter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @SpringBootTest
 class TastefulAiApplicationTests {
@@ -9,5 +14,18 @@ class TastefulAiApplicationTests {
     @Test
     void contextLoads() {
     }
+
+    @MockBean
+    private RedisMessageListenerContainer redisMessageListenerContainer;
+
+    @MockBean
+    private JwtAuthFilter jwtAuthFilter;
+
+    @MockBean
+    private OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @MockBean
+    private CustomOAuth2UserService customOAuth2UserService;
+
 
 }
